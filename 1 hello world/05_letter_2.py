a80ab4e2319b875c0a4e469f6454a2417c309c9a
ksjadhav <100179580+ksjadhav@users.noreply.github.com>
2025-07-17 19:20:14 +0530

    Add files via upload

---
+++ 1 hello world/05_letter_2.py
@@ -0,0 +1,19 @@
+# Snail Mail 💌
+# Codédex
+
+print('+---------------------------------------------------------------------+')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|        Wrote myself a letter                                        |')
+print('|        just a single question                                       |')
+print('|                                                                     |')
+print('|        "When you finally get this, where will you be?" ✨           |')
+print('|                                                                     |')
+print('|        Will you be a shipwreck or a star                            |')
+print('|        Falling for a boy who doesn\'t play guitar?                  |')
+print('|        Now rewind the tape back to the start                        |')
+print('|                                                                     |')
+print('|                                          - Caroline Polachek        |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('+---------------------------------------------------------------------+')
