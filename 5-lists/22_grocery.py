40f50113eb22a5c170a6e9f055ba604f692e66a5
ksjadhav <100179580+ksjadhav@users.noreply.github.com>
2025-07-22 23:46:55 +0530

    Add files via upload

---
+++ 5-lists/22_grocery.py
@@ -0,0 +1,11 @@
+# Grocery List 🛒
+# Codédex
+
+grocery = ['🥚 Eggs', 
+           '🥑 Avocados', 
+           '🍪 Cookies', 
+           '🌶 Hot Pepper Jam', 
+           '🫐 Blueberries', 
+           '🥦 Broccoli']
+
+print(grocery)
