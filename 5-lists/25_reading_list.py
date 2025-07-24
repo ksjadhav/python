a411613ec68d7f00c387a0e2bc2394ddf372e6a8
ksjadhav <100179580+ksjadhav@users.noreply.github.com>
2025-07-24 23:39:53 +0530

    Add files via upload

---
+++ 5-lists/25_reading_list.py
@@ -0,0 +1,16 @@
+# Reading List 📚
+# Codédex
+
+books = ['Harry Potter',
+         '1984',
+         'The Fault in Our Stars',
+         'The Mom Test',
+         'Life in Code']
+
+print(books)
+
+books.append('Pachinko')
+books.remove('The Fault in Our Stars')
+books.pop(1)
+
+print(books)
---
+++ 5-lists/26_mixtape.py
@@ -0,0 +1,12 @@
+# Mixtape: Classical Relaxation Theme 💿
+# Codédex
+
+playlist = ['Johannes Brahms - Symphony No.1 in C Minor, Op. 68', 
+            'Erik Satie - Gymnopédie No.1', 
+            'Tchaikovsky - 1812 Overture',
+            'Edvard Grieg - In the Hall of the Mountain King',
+            'Ludwig van Beethoven - Moonlight Sonata', 
+            'Antonio Vivaldi - The Four Seasons']
+
+for song in playlist:
+  print(song)
